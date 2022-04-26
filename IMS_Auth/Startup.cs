499ee69716c196892b5ca908cd82d6ee1d1a39ea
DeepRamoliya <dramoliya@aakashinformatics.com>
2022-04-26 18:19:30 +0530

    Add project files.

---
+++ IMS_Auth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IMS_Auth.Startup))]
+namespace IMS_Auth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
