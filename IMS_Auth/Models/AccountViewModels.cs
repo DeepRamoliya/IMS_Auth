cfd317cf74cefe987d39dce60460f4606d0d7655
DevSonuPatidar <102738088+DevSonuPatidar@users.noreply.github.com>
2022-05-04 18:21:28 +0530

    Added auto createdby and updatedby value picker

--- IMS_Auth/Models/AccountViewModels.cs
+++ IMS_Auth/Models/AccountViewModels.cs
@@ -65,6 +65,9 @@ namespace IMS_Auth.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
